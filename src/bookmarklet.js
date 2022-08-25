f96cd4b6354b1bd8803ac89a27da38f836766147
MarcusWilkinson <wilkinsonmarcus@gmail.com>
2022-08-25 11:54:06 +0100

    Hierarchy testing to build unique selector. Max 1 parent

---
+++ src/bookmarklet.js
@@ -0,0 +1,5 @@
+javascript: (function () {
+    var jsCode = document.createElement('script');
+    jsCode.setAttribute('src', "https://storage.googleapis.com/deltek/show-ids.js");
+    document.body.appendChild(jsCode);
+}());
--- src/show-ids.js
+++ src/show-ids.js
@@ -1,136 +1,173 @@
-const DATA_ID_BORDER_CLASS = 'dm-data-id-border';
-const DATA_ID_INSET_CLASS = 'dm-data-id-inset';
-const DM_COLOUR_CLASS = 'dm-colour-modifier';
+(function() {
+    const DATA_ID_BORDER_CLASS = 'dm-data-id-border';
+    const DATA_ID_INSET_CLASS = 'dm-data-id-inset';
+    const DM_COLOUR_CLASS = 'dm-colour-modifier';
 
-function addScript(scriptPath) {
-    return new Promise((resolve) => {
-        javascript: (function(e, s) {
-            e.src = s;
-            e.onload = resolve;
-            document.head.appendChild(e);
-        })(document.createElement('script'), scriptPath);
-    });
-}
-
-function loadJQuery() {
-    return new Promise((resolve, reject) => {
-        addScript('//code.jquery.com/jquery-latest.min.js').then(() => {
-            jQuery.noConflict();
-            console.log('jQuery injected');
-            resolve();
+    function addScript(scriptPath) {
+        return new Promise((resolve) => {
+            javascript: (function (e, s) {
+                e.src = s;
+                e.onload = resolve;
+                document.head.appendChild(e);
+            })(document.createElement('script'), scriptPath);
         });
-    });
-}
+    }
 
-function loadTippy() {
-    console.log('Loading tippy...');
-    return new Promise((resolve, reject) => {
-        addScript('https://unpkg.com/@popperjs/core@2')
-            .then(()=> {
-                return addScript('https://unpkg.com/tippy.js@6')
-            })
-            .then(() => {
-                console.log('Tippy injected');
+    function loadJQuery() {
+        return new Promise((resolve, reject) => {
+            addScript('//code.jquery.com/jquery-latest.min.js').then(() => {
+                jQuery.noConflict();
+                console.log('jQuery injected');
                 resolve();
             });
-    });
-}
-
-function clearExistingDivs(divs) {
-    return new Promise((resolve, reject) => {
-        const $ = jQuery.noConflict();
-        $(`.${DATA_ID_INSET_CLASS}, .${DATA_ID_BORDER_CLASS}`).remove();
-        resolve();
-    });
-}
-
-function findAllDMElements() {
-    return new Promise((resolve, reject) => {
-        const $ = jQuery.noConflict();
-        const filtered = $('*').filter(function() {
-            return $(this).prop('nodeName').indexOf('DM') === 0
         });
-        resolve(filtered);
-    });
-}
-
-function findAllDataIdElements() {
-    return new Promise((resolve, reject) => {
-        const $ = jQuery.noConflict();
-        console.log(`looking for DataId divs`);
-        const result = $('[dm-data-id],[dmDataId]');
-        resolve(result);
-    });
-}
-
-function copyToClipboard(str) {
-    console.log(`Copying to clipboard: ${str}`);
-    navigator.clipboard.writeText(str);
-}
-
-function processDiv(element) {
-    const $ = jQuery.noConflict();
-    const pos = element.getBoundingClientRect();
-    if (pos.width === 0 && pos.height ===0) {
-        return;
-    }
-    let hasDataId = true;
-    const div = $(element);
-    // div.addClass(HIGHLIGHT_CLASS_NAME);
-    let dataId = div.attr('dm-data-id');
-    if (!dataId) {
-        dataId = div.attr('dmDataId');
-    } if (!dataId) {
-        hasDataId = false;
-        const nodeName = div.prop('nodeName');
-        dataId = nodeName.indexOf('DM') === 0?nodeName:null;
     }
 
-    // border rect
-    const popoverBorder = window.document.createElement('div');
-    $(popoverBorder)
-        .addClass(DATA_ID_BORDER_CLASS)
-        .attr('style', `position: fixed; left:${pos.left}px; top:${pos.top}px; width:${pos.width}px; height: ${pos.height}px; z-index:9999`);
-
-
-    const popoverInset = window.document.createElement('div');
-    $(popoverInset)
-        .addClass(DATA_ID_INSET_CLASS)
-        .attr('style', `position: fixed; left:${pos.left}px; top:${pos.top}px; z-index:9999`);
-    document.body.prepend(popoverInset);
-    document.body.prepend(popoverBorder);
-
-    if (!hasDataId) {
-        $(popoverBorder).addClass(DM_COLOUR_CLASS);
-        $(popoverInset).addClass(DM_COLOUR_CLASS)
-    }
-    // info square
-    const link = window.document.createElement('p');
-    $(link).addClass('tippy-link').html(dataId).on( "click", () => copyToClipboard(dataId));
-    tippy(popoverInset, {
-        content: link,
-        appendTo: document.body,
-        interactive: true,
-        theme: 'maconomy',
-        allowHTML: true
-    });
-}
-
-function annotateDivs(divs) {
-    const $ = jQuery.noConflict();
-    return new Promise((resolve, reject) => {
-        console.log(`Annotating ${divs.length} divs`);
-        divs.each(function() {
-            processDiv(this);
+    function loadTippy() {
+        console.log('Loading tippy...');
+        return new Promise((resolve, reject) => {
+            addScript('https://unpkg.com/@popperjs/core@2')
+                .then(() => {
+                    return addScript('https://unpkg.com/tippy.js@6')
+                })
+                .then(() => {
+                    console.log('Tippy injected');
+                    resolve();
+                });
         });
-    });
-}
+    }
 
-function createCSSClasses() {
-    return new Promise((resolve, reject) => {
+    function clearExistingDivs(divs) {
+        return new Promise((resolve, reject) => {
+            const $ = jQuery.noConflict();
+            $(`.${DATA_ID_INSET_CLASS}, .${DATA_ID_BORDER_CLASS}`).remove();
+            resolve();
+        });
+    }
+
+    function findAllDMElements() {
+        return new Promise((resolve, reject) => {
+            const $ = jQuery.noConflict();
+            const filtered = $('*').filter(function () {
+                return $(this).prop('nodeName').indexOf('DM') === 0
+            });
+            resolve(filtered);
+        });
+    }
+
+    function findAllDataIdElements() {
+        return new Promise((resolve, reject) => {
+            const $ = jQuery.noConflict();
+            console.log(`looking for DataId divs`);
+            const result = $('[dm-data-id],[dmDataId]');
+            resolve(result);
+        });
+    }
+
+    function copyToClipboard(leafElement) {
         const $ = jQuery.noConflict();
-        $('<style/>', {
-            text: `
+        const leaf = $(leafElement);
+        const parents = [];
+        leaf.parents().each(function() {parents.push(this)});
+        const selector = buildSearch(leaf, parents);
+        console.log(`Copying selector "${selector}" to clipboard`);
+        navigator.clipboard.writeText(selector);
+    }
+
+    function buildSearch(leaf, parents) {
+        if (hasSingleItem([leaf])) {
+            return getQueryForElement(leaf);
+        }
+        for (const parent of parents) {
+            const candidateChain = [parent, leaf];
+            if (hasSingleItem(candidateChain)) {
+                return candidateChain.map(getQueryForElement).join(' ');
+            }
+        }
+        return getQueryForElement(leaf);
+    }
+
+    function hasSingleItem(items) {
+        const $ = jQuery.noConflict();
+        return $(items.map(getQueryForElement).join(' ')).length == 1;
+    }
+
+    function getQueryForElement(item) {
+        const $ = jQuery.noConflict();
+        if ($(item).attr('dm-data-id')) {
+            return `[dm-data-id="${(item).attr('dm-data-id')}"]`;
+        }
+        if ($(item).attr('dmDataId')) {
+            return `[dmDataId="${(item).attr('dm-data-id')}"]`;
+        }
+        return $(item).prop('nodeName');
+    }
+
+    function processDiv(element) {
+        const $ = jQuery.noConflict();
+        const pos = element.getBoundingClientRect();
+        if (pos.width === 0 && pos.height === 0) {
+            return;
+        }
+        let hasDataId = true;
+        const div = $(element);
+        // div.addClass(HIGHLIGHT_CLASS_NAME);
+        let dataId = div.attr('dm-data-id');
+        if (!dataId) {
+            dataId = div.attr('dmDataId');
+        }
+        if (!dataId) {
+            hasDataId = false;
+            const nodeName = div.prop('nodeName');
+            dataId = nodeName.indexOf('DM') === 0 ? nodeName : null;
+        }
+
+        // border rect
+        const popoverBorder = window.document.createElement('div');
+        $(popoverBorder)
+            .addClass(DATA_ID_BORDER_CLASS)
+            .attr('style', `position: fixed; left:${pos.left}px; top:${pos.top}px; width:${pos.width}px; height: ${pos.height}px; z-index:9999`);
+
+
+        const popoverInset = window.document.createElement('div');
+        $(popoverInset)
+            .addClass(DATA_ID_INSET_CLASS)
+            .attr('style', `position: fixed; left:${pos.left}px; top:${pos.top}px; z-index:9999`);
+        document.body.prepend(popoverInset);
+        document.body.prepend(popoverBorder);
+
+        if (!hasDataId) {
+            $(popoverBorder).addClass(DM_COLOUR_CLASS);
+            $(popoverInset).addClass(DM_COLOUR_CLASS)
+        }
+        // info square
+        const link = window.document.createElement('p');
+
+        $(link).addClass('tippy-link').html(dataId).on("click", () => copyToClipboard(element));
+        tippy(popoverInset, {
+            content: link,
+            appendTo: document.body,
+            interactive: true,
+            theme: 'maconomy',
+            allowHTML: true
+        });
+    }
+
+    function annotateDivs(divs) {
+        const $ = jQuery.noConflict();
+        return new Promise((resolve, reject) => {
+            console.log(`Annotating ${divs.length} divs`);
+            divs.each(function () {
+                processDiv(this);
+            });
+        });
+    }
+
+    function createCSSClasses() {
+        return new Promise((resolve, reject) => {
+            const $ = jQuery.noConflict();
+            $('<style/>', {
+                text: `
     .${DATA_ID_BORDER_CLASS} {
       border: #ff0 solid 2px;
       pointer-events: none;
@@ -157,16 +194,18 @@ function createCSSClasses() {
       cursor: pointer;
     }
     `,
-        }).appendTo('head');
-        resolve();
-    });
-}
+            }).appendTo('head');
+            resolve();
+        });
+    }
 
-loadJQuery()
-    .then(loadTippy)
-    .then(createCSSClasses)
-    .then(clearExistingDivs)
-    .then(findAllDataIdElements)
-    .then(annotateDivs)
-    .then(findAllDMElements)
-    .then(annotateDivs);
+    loadJQuery()
+        .then(loadTippy)
+        .then(createCSSClasses)
+        .then(clearExistingDivs)
+        .then(findAllDataIdElements)
+        .then(annotateDivs)
+        .then(findAllDMElements)
+        .then(annotateDivs);
+
+}) ();
